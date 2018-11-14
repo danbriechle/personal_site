681c0627703e8f0336452e8bbb8f4dd21a0b67a2
Daniel Briechle <danbriechle@hotmail.com>
2018-11-14 12:06:06 -0700

    Created route for about page and error page. Test passing

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,23 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
+    case env["PATH_INFO"]
+    when '/' then index
+    when '/about' then about
+    else
+      error
+    end
+  end
+
+  def self.index
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.about
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/about.html')]]
+  end
+
+  def self.error
+    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 end
--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -3,7 +3,22 @@ require './test/test_helper'
 class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
+    # save_and_open_page
     assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+
+  def test_user_sees_error_for_non_existent_page
+    visit '/savings_acount'
+    # save_and_open_page
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+
+  end
+
+  def test_user_can_see_the_about_page
+    visit '/about'
+    assert page.has_content?("About")
     assert_equal 200, page.status_code
   end
 end
